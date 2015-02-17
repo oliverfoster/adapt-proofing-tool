37751167b2bdb612b17ef42a8534bd637ac6becc
Oliver Foster <oliver.foster@kineo.com>
2015-02-17 16:22:03 +0000

    Initial

---
+++ index.js
@@ -0,0 +1,72 @@
+var stringify = require('csv-stringify'),
+	inquirer = require("inquirer"),
+	_ = require("underscore"),
+	Backbone = require("backbone"),
+	path = require("path"),
+	fs = require("fs");
+var htmlToText = require('html-to-text');
+
+var cwd = process.cwd();
+
+var data = {
+	config: new (Backbone.Collection.extend({}))(require( path.join(cwd, "config.json") )),
+	course: new Backbone.Collection(require( path.join(cwd, "en/course.json") )),
+	articles: new Backbone.Collection(require( path.join(cwd, "en/articles.json") )),
+	blocks: new Backbone.Collection(require( path.join(cwd, "en/blocks.json") )),
+	components: new Backbone.Collection(require( path.join(cwd, "en/components.json") )),
+	contentObjects: new Backbone.Collection(require( path.join(cwd, "en/components.json") ))
+};
+
+var proofing = [];
+
+data.components.each(function(item, index) {
+	proofing = proofing.concat( getComponent(item) );
+});
+
+
+stringify(proofing, function(err, output){
+
+	fs.writeFileSync("proofing.csv", output);
+
+});
+
+
+function getComponent(item) {
+	var id = item.get("_id");
+	var title = item.get("title") || "--N/A--";
+	var displayTitle = item.get("displayTitle") || "--N/A--";
+	var body = item.get("body") || "--N/A--";
+
+	title = htmlToText.fromString(title);
+	displayTitle = htmlToText.fromString(displayTitle);
+	body = htmlToText.fromString(body);
+
+	var rtn = [
+		[ "COMPONENT", id ],
+		[ "", "TITLE", title ],
+		[ "", "DISPLAY TITLE", displayTitle ],
+		[ "", "BODY", body ],
+		[]
+	];
+
+	rtn = rtn.concat(getType(item));
+
+	return rtn;
+}
+
+function getType(component) {
+	var ret = [];
+	switch (component.get("_component")) {
+	case "mcq":
+		var items = component.get("_items")
+		if (!items) break;
+		ret.push( [ "", items.length + " ITEMS" ] );
+		_.each(items, function(item, i) {
+			var text = item.text;
+			text = htmlToText.fromString(text);
+			ret.push(["",text])
+		});
+		ret.push([]);
+	}
+	return ret;
+}
